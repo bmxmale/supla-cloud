7bd5a50bb1bd20437921eaedc89c56ca4a383428
Wojciech Frącz <fraczwojciech@gmail.com>
2019-06-17 01:08:46 +0200

    Upgrade dependencies after declaring PHP version >= 7.1.3

--- src/SuplaBundle/Controller/Api/UserController.php
+++ src/SuplaBundle/Controller/Api/UserController.php
@@ -94,7 +94,7 @@ class UserController extends RestController {
         int $ioDevicesRegistrationEnableTime,
         bool $requireRegulationsAcceptance,
         bool $recaptchaEnabled,
-        $recaptchaSecret,
+        ?string $recaptchaSecret,
         array $availableLanguages,
         bool $accountsRegistrationEnabled,
         bool $mqttBrokerEnabled,
@@ -265,7 +265,7 @@ class UserController extends RestController {
         if ($server->isLocal()) {
             return $this->accountCreateAction($request);
         } else {
-            list($response, $status) = $this->suplaCloudRequestForwarder->registerUser($server, $request);
+            [$response, $status] = $this->suplaCloudRequestForwarder->registerUser($server, $request);
             return $this->view($response, $status);
         }
     }
@@ -299,7 +299,7 @@ class UserController extends RestController {
             if ($targetCloud->isLocal()) {
                 $enabled = $this->userManager->userByEmail($username)->isEnabled();
             } else {
-                list($response,) = $this->suplaCloudRequestForwarder->getUserInfo($targetCloud, $username);
+                [$response,] = $this->suplaCloudRequestForwarder->getUserInfo($targetCloud, $username);
                 $enabled = $response ? ($response['enabled'] ?? false) : false;
             }
             return $this->view(
@@ -374,7 +374,7 @@ class UserController extends RestController {
             if ($request->getMethod() == Request::METHOD_PATCH) {
                 $server = $this->autodiscover->getAuthServerForUser($username);
                 if (!$server->isLocal()) {
-                    list($content, $status) = $this->suplaCloudRequestForwarder->resendActivationEmail($server, $username);
+                    [$content, $status] = $this->suplaCloudRequestForwarder->resendActivationEmail($server, $username);
                     return new JsonResponse($content, $status);
                 }
             }
@@ -427,7 +427,7 @@ class UserController extends RestController {
         if (preg_match('/@/', $username) || $token) {
             if ($request->getMethod() == Request::METHOD_PATCH) {
                 $server = $this->autodiscover->getAuthServerForUser($username);
-                list(, $status) = $this->suplaCloudRequestForwarder->resetPasswordToken($server, $username);
+                [, $status] = $this->suplaCloudRequestForwarder->resetPasswordToken($server, $username);
                 Assertion::eq($status, Response::HTTP_OK, 'Could not reset the password.'); // i18n
             } elseif ($request->getMethod() == Request::METHOD_POST) {
                 $user = $this->userManager->userByEmail($username);
